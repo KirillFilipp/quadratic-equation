1213b6185a32a787d3e77f0606a4779df07e234f
KirillFilipp <kirill_filipp@mail.ru>
2018-09-27 22:31:32 +0200

    '1'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let x1, x2, solutions;
+  let m = equation.split (' ');
+  let a=m[0];
+  let b=m[3]+m[4];
+  let c=m[7]+m[8];
+  x1=(-b+(Math.sqrt(Math.pow(b,2)-4*a*c)))/(2*a); 
+  x2=(-b-(Math.sqrt(Math.pow(b,2)-4*a*c)))/(2*a);
+  if (x1 < x2) {
+    solutions = [Math.round(x1),Math.round(x2)];
+  } else {
+    solutions = [Math.round(x2),Math.round(x1)];
+  }
+return solutions;
 }
